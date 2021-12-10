00e9d906a7c9f5de44660665db94ad13c4858c95
Mosa M.Sebdani <mosam.sebdani@mosas-mbp-2.home>
2021-12-10 18:56:44 +0100

    init

---
+++ src/lib/Counter.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+  let count: number = 0
+
+  const increment = () => {
+    count += 1
+  }
+
+  const remove = () => {
+    count -= 1
+  }
+
+  $:doubled = count * 2
+
+  $:if (count >= 10) {
+	alert(`count is dangerously high!`);
+	count = 9;
+}
+</script>
+
+<button on:click={increment}>
+  Clicks: {count}
+  <br>
+  Doubled: {doubled}
+</button>
+
+{#if count > 0}
+<button on:click={remove}>
+  Remove 
+</button>
+{/if}
+
+<style>
+  button {
+    font-family: inherit;
+    font-size: inherit;
+    padding: 1em 2em;
+    color: #ff3e00;
+    background-color: rgba(255, 62, 0, 0.1);
+    border-radius: 2em;
+    border: 2px solid rgba(255, 62, 0, 0);
+    outline: none;
+    width: 200px;
+    font-variant-numeric: tabular-nums;
+    cursor: pointer;
+  }
+
+  button:focus {
+    border: 2px solid #ff3e00;
+  }
+
+  button:active {
+    background-color: rgba(255, 62, 0, 0.2);
+  }
+</style>
---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import App from './App.svelte'
+
+const app = new App({
+  target: document.getElementById('app'),
+  props: {
+		answer: 42
+	}
+})
+
+export default app
