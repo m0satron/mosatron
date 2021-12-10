5e27d7df4d9780ddc5e996c72d7030397dbe7607
m0satron <18460264+m0satron@users.noreply.github.com>
2021-12-10 19:40:36 +0100

    fix: add procfile

--- src/App.svelte
+++ src/App.svelte
@@ -1,23 +1,21 @@
 <script lang="ts">
-  import logo from './assets/svelte.png'
-  import Counter from './lib/Counter.svelte'
+  // import Counter from './lib/Counter.svelte'
 </script>
 
 <main>
-  <img src={logo} alt="Svelte Logo" />
-  <h1>Hello Typescript!</h1>
+  <!-- <img src={logo} alt="Svelte Logo" /> -->
+  <h1>Hello!</h1>
 
-  <Counter />
+  <!-- <Counter /> -->
 
   <p>
-    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
-    apps.
+   This is my site
   </p>
 
-  <p>
+  <!-- <p>
     Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
     the officially supported framework, also powered by Vite!
-  </p>
+  </p> -->
 </main>
 
 <style>
