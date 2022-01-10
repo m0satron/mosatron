2337da399421066a5dfb27e867bb4a8808c0c1a1
m0satron <18460264+m0satron@users.noreply.github.com>
2022-01-10 18:17:47 +0100

    feat: add gallery

--- src/App.svelte
+++ src/App.svelte
@@ -1,63 +1,96 @@
 <script lang="ts">
-  // import Counter from './lib/Counter.svelte'
+  import Counter from "./lib/Counter.svelte";
+  import MainCard from "./lib/MainCard.svelte";
+
+  const photos = [
+    {
+      id: "9160f315-387c-4cda-b67c-474f343afc36",
+      title: "Colors are fun 2",
+    },
+    {
+      id: "e37e611d-fefb-4a6d-bef2-40fbef0893e0",
+      title: "Things etc",
+    },
+    {
+      id: "41e84568-a090-4660-b11d-6812cf0edb96",
+      title: "Fake moray and stuff",
+    },
+    {
+      id: "112bb03b-f003-460f-ad52-4ab696888a12",
+      title: "Why is reality escaping entropy",
+    },
+    {
+      id: "c70f3058-0c5a-4f48-a48b-1c760434bdb1",
+      title: "Colors are fun",
+    },
+  ];
+  $: pic = 0;
+  $: img = {
+    src: `https://ucarecdn.com/${photos[pic].id}/-/preview/600x600/-/quality/best/`,
+    alt: photos[pic].title,
+  };
+
+  setInterval(() => {
+    pic = Math.floor(Math.random() * photos.length);
+  }, 5000);
 </script>
 
-<main>
-  <!-- <img src={logo} alt="Svelte Logo" /> -->
-  <h1>Hello!</h1>
+<div class="main">
+  <div class="title-wrapper">
+    <h3 class="title">Mosa Mazroui-Sebdani</h3>
+    <h3 class="title">{img.alt}</h3>
+  </div>
+  <MainCard>
+    <img src={img.src} alt={img.alt} />
+  </MainCard>
+</div>
 
-  <!-- <Counter /> -->
+<style lang="sass">
 
-  <p>
-   This is my site
-  </p>
+  .main
+    display: flex
+    flex-direction: column
+    align-items: center
 
-  <!-- <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
-    the officially supported framework, also powered by Vite!
-  </p> -->
-</main>
+  .title
+    color: white
+    font-weight: 200
+  
+  .title-wrapper
+    display: flex
+    width: 600px
+    justify-content: space-between
+    
+  :root 
+    font-family: Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif
+    background-color: #c5decd
 
-<style>
-  :root {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  }
+  // main 
+  //   text-align: center
+  //   padding: 1em
+  //   margin: 0 auto
 
-  main {
-    text-align: center;
-    padding: 1em;
-    margin: 0 auto;
-  }
+  // h1 
+  //   color: #ff3e00
+  //   text-transform: uppercase
+  //   font-size: 4rem
+  //   font-weight: 100
+  //   line-height: 1.1
+  //   margin: 2rem auto
+  //   max-width: 14rem
+  
 
-  /* img {
-    height: 16rem;
-    width: 16rem;
-  } */
+  p 
+    max-width: 14rem
+    margin: 1rem auto
+    line-height: 1.35
+  
 
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4rem;
-    font-weight: 100;
-    line-height: 1.1;
-    margin: 2rem auto;
-    max-width: 14rem;
-  }
+  @media (min-width: 480px) 
+    // h1 
+    //   max-width: none
+    
 
-  p {
-    max-width: 14rem;
-    margin: 1rem auto;
-    line-height: 1.35;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      max-width: none;
-    }
-
-    p {
-      max-width: none;
-    }
-  }
+    p 
+      max-width: none
 </style>
---
+++ src/lib/MainCard.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+</script>
+
+<div class="main-card">
+  <slot />
+</div>
+
+<style lang="sass">
+
+.main-card
+  margin: 12px
+  height: 600px
+  width: 600px
+  border-radius: 6px
+  padding: 6px
+  -webkit-box-shadow: 0px 0px 50px -7px rgba(0,0,0,0.55)
+  box-shadow: 0px 0px 50px -7px rgba(0,0,0,0.55)
+  background-color: white
+</style>
